4140d9f7ae504170255c5aa5bbeb9087be326d2e
ihascats <s.pavlovic.rs@gmail.com>
2023-01-16 18:39:11 +0100

    Fix nav element being vertically scrollable

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,8 @@ export default function Home() {
       </Head>
       <main>
         <div className="flex flex-col-reverse h-screen">
-          <nav className="overflow-auto bg-green-400">
-            <ul className="flex">
+          <nav className="overflow-x-auto bg-green-400 h-[43px]">
+            <ul className="flex h-fit">
               <li className="px-4 py-2 hover:bg-white/30">Wishlist</li>
               <li className="px-4 py-2 hover:bg-white/30">Applied</li>
               <li className="px-4 py-2 hover:bg-white/30">Rejected</li>
