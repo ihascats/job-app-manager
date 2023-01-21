aa2944c56beee5d85a1a285a19ea6be146bc1f90
ihascats <s.pavlovic.rs@gmail.com>
2023-01-21 23:24:19 +0100

    Add a loading svg

---
+++ components/icons.js
@@ -0,0 +1,8 @@
+export default function Icons() {
+  const loading = (
+    <svg className="w-12 h-12 fill-neutral-400 loading" viewBox="0 0 24 24">
+      <path fill="inherit" d="M12,4V2A10,10 0 0,0 2,12H4A8,8 0 0,1 12,4Z" />
+    </svg>
+  );
+  return { loading };
+}
