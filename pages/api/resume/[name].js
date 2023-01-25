6d607b28b51fb5f33c48d0bd769297b650667108
ihascats <s.pavlovic.rs@gmail.com>
2023-01-25 23:35:48 +0100

    Add a route 4 downloading & deleting resume files

---
+++ pages/api/resume/[name].js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+export default async function handler(req, res) {
+  const { name } = req.query;
+  const directoryPath = `./uploads/${process.env.USERNAME}/resume`;
+  const filePath = path.join(directoryPath, name);
+
+  if (req.method === 'GET') {
+    const buffer = fs.readFileSync(filePath);
+    res.send(buffer);
+  }
+
+  if (req.method === 'DELETE') {
+    fs.unlink(filePath, (err) => {
+      if (err) throw err;
+      console.log('path/file.txt was deleted');
+      res.send({ status: 'success' });
+    });
+  }
+}
