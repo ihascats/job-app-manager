12e51a7be8c2c55231cd62aaf4304f7cb81226c0
ihascats <s.pavlovic.rs@gmail.com>
2023-01-25 18:56:43 +0100

    Add non-function download & delete btn 4 resumes

--- components/resumeCard.js
+++ components/resumeCard.js
@@ -1,10 +1,20 @@
+import Icons from './icons';
 import Timestamp from './timestamp';
 
 export default function ResumeCard({ resume }) {
+  const icons = Icons();
   return (
     <div className="rounded-xl p-2 bg-white h-fit w-screen-p4">
       <h1 className={`font-bold truncate`}>{resume.name}</h1>
       <Timestamp createdAt={resume.createdAt} />
+      <div className="w-full grid grid-cols-2">
+        <button className="bg-red-500 rounded-bl-lg py-1 fill-white flex justify-center">
+          {icons.deleteFile}
+        </button>
+        <button className="bg-green-500 rounded-br-lg py-1 fill-white flex justify-center">
+          {icons.download}
+        </button>
+      </div>
     </div>
   );
 }
