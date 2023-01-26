06d84efcb4cc532faf6610f11a0f66de016290f0
ihascats <s.pavlovic.rs@gmail.com>
2023-01-26 23:58:49 +0100

    Add a sign out button

--- components/toggleNewButtons.js
+++ components/toggleNewButtons.js
@@ -1,11 +1,13 @@
+import { signOut } from 'next-auth/react';
 import { useRef } from 'react';
+import Icons from './icons';
 
 export default function ToggleNewButtons({
   setCreateNewEntry,
   setButtonsVisible,
   getResumeList,
 }) {
-  async function addResume(event) {
+  async function addResume() {
     const formData = new FormData(form.current);
     const link = `/api/uploadResume`;
     await fetch(link, {
@@ -15,6 +17,7 @@ export default function ToggleNewButtons({
     getResumeList();
   }
   const form = useRef();
+  const icons = Icons();
 
   return (
     <div className="flex flex-col absolute bottom-12 right-2 items-end gap-4">
@@ -27,12 +30,18 @@ export default function ToggleNewButtons({
       >
         +
       </button>
-      <form ref={form} onChange={addResume} className="h-9">
+      <form ref={form} onChange={addResume} className="h-[35px] pt-1">
         <label className="p-2 bg-green-600 text-sm rounded-md">
           Add Resume
           <input hidden name="resume" type="file"></input>
         </label>
       </form>
+      <button
+        className="p-2 bg-green-600 text-sm rounded-md flex gap-1 h-9 items-center"
+        onClick={() => signOut()}
+      >
+        Sign out{icons.signOut}
+      </button>
     </div>
   );
 }
