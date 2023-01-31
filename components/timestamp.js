e9eaaa4504ceb96c061fb6fdf0056256ef929d06
ihascats <s.pavlovic.rs@gmail.com>
2023-01-31 21:12:56 +0100

    Fix timestamp

--- components/timestamp.js
+++ components/timestamp.js
@@ -3,14 +3,9 @@ export default function Timestamp({ createdAt }) {
   const pastDate = new Date(createdAt);
   const difference = Math.abs(currentDate - pastDate) / 36e5;
 
-  const calcSeconds = Math.floor(difference * 3600);
-  const calcMinutes = Math.floor(difference * 60);
-  const calcHours = Math.floor(difference);
-
-  // issues with sql timestamp require the conditional, if time isn't an sql timestamp it'll be 1 (s, m, h, d) behind after the first (s, m, h, d)
-  const seconds = calcSeconds - 3600 < 0 ? calcSeconds : calcSeconds - 3600;
-  const minutes = calcMinutes - 60 < 0 ? calcMinutes : calcMinutes - 60;
-  const hours = calcHours - 1 < 0 ? calcHours : calcHours - 1;
+  const seconds = Math.floor(difference * 3600);
+  const minutes = Math.floor(difference * 60);
+  const hours = Math.floor(difference);
   const days = Math.floor(difference / 24);
 
   const timeAmount =
--- pages/api/[user]/addNewEntry.js
+++ pages/api/[user]/addNewEntry.js
@@ -35,6 +35,33 @@ export default async function handler(req, res) {
     });
   });
 
+  // https://stackoverflow.com/a/17415677/19683372
+  function toIsoString(date) {
+    var tzo = -date.getTimezoneOffset(),
+      dif = tzo >= 0 ? '+' : '-',
+      pad = function (num) {
+        return (num < 10 ? '0' : '') + num;
+      };
+
+    return (
+      date.getFullYear() +
+      '-' +
+      pad(date.getMonth() + 1) +
+      '-' +
+      pad(date.getDate()) +
+      'T' +
+      pad(date.getHours()) +
+      ':' +
+      pad(date.getMinutes()) +
+      ':' +
+      pad(date.getSeconds()) +
+      dif +
+      pad(Math.floor(Math.abs(tzo) / 60)) +
+      ':' +
+      pad(Math.abs(tzo) % 60)
+    );
+  }
+
   async function insertData({
     status,
     company,
@@ -46,8 +73,7 @@ export default async function handler(req, res) {
     resume,
     cover,
   }) {
-    const createdAt = `${new Date()
-      .toISOString()
+    const createdAt = `${toIsoString(new Date())
       .slice(0, 19)
       .replace('T', ' ')}`;
     connection.query(
